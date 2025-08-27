7f3a80672b85d86263568f2e5815b16e966c8543
samanta181 <mogrovejosamanta@gmail.com>
2025-08-27 00:27:36 -0500

    my-witgets-page

--- lib/pages/education_page.dart
+++ lib/pages/education_page.dart
@@ -344,6 +344,8 @@ class _EducationScreenState extends State<EducationScreen>
                                 fontWeight: FontWeight.bold,
                                 color: Color(0xFF2D3748),
                               ),
+                               maxLines: 3, 
+                                 overflow: TextOverflow.ellipsis, 
                             ),
                             SizedBox(height: 4),
                             Text(
@@ -353,6 +355,8 @@ class _EducationScreenState extends State<EducationScreen>
                                 fontWeight: FontWeight.w600,
                                 color: education['color'],
                               ),
+                                maxLines: 2, 
+                                 overflow: TextOverflow.ellipsis, 
                             ),
                           ],
                         ),
